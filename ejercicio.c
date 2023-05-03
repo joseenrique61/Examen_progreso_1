ca45c6c4ea06fc660b405494e047d13c21f1c51c
joseenrique61 <joseenrique61@hotmail.com>
2023-05-03 15:31:15 -0500

    Programa finalizado

--- ejercicio.c
+++ ejercicio.c
@@ -1,7 +1,7 @@
 // Construya un programa para hallar, de un conjunto de N números, qué porcentaje son cero, qué porcentaje son positivos y el porcentaje de números negativos.
 #include <stdio.h>
-#include <iostream>
 
+//Se declaran las variables a usar.
 int numerosPositivos = 0;
 int numerosNegativos = 0;
 int ceros = 0;
@@ -11,13 +11,16 @@ void ObtenerNumeros(int cantidadNumeros);
 
 int main()
 {
+    //Se le pide al usuario la cantidad de números a ingresar.
     int cantidadNumeros = 0;
     printf("Ingrese la cantidad de numeros a leer: ");
     scanf("%i", &cantidadNumeros);
+
+    //Se llama a la función ObtenerNumeros para sacar los números de cada tipo.
     ObtenerNumeros(cantidadNumeros);
-    printf("%i", numerosPositivos);
-    printf("%i", numerosNegativos);
-    printf("%i", ceros);
+    
+    //Se imprimen los números positivos, negativos y ceros
+    printf("Positivos: %f porciento, Ceros: %f porciento, Negativos: %f porciento", numerosPositivos/count * 100, ceros/count * 100, numerosNegativos/count * 100);
 }
 
 //Función usada para encontrar los números positivos, negativos y ceros. Itera a través de cada número y determina de qué tipo es, luego añade uno a la variable correspondiente.
@@ -42,5 +45,3 @@ void ObtenerNumeros(int cantidadNumeros)
         }
     }
 }
-
-// float Porcentajes(int[] )
